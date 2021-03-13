20a83974eee186d47ab8b26f5960d2b231a90f29
ramjak <rama.jakaria@gmail.com>
2021-03-13 10:46:09 +0700

    feat: add AppBar to BasePage.tsx

--- src/components/BasePage.tsx
+++ src/components/BasePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from "./BasePage.module.scss";
 import logo from "../logo.svg";
-import {Card} from "@material-ui/core";
+import {AppBar, Button, Card, Toolbar, Typography} from "@material-ui/core";
 
 interface IBasePage {
   children: React.ReactNode
@@ -11,6 +11,15 @@ function BasePage(props: IBasePage) {
   const { children } = props;
   return (
     <div className={styles.App}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={styles.appTitle}>
+            Sample App
+          </Typography>
+          <Button color="inherit">Todo</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <header className={styles.AppHeader}>
         <img src={logo} className={styles.AppLogo} alt="logo" />
         <Card className={styles.WrapperCard}>
