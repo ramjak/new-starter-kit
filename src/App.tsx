ec3df2e42c6cd51405068374150349743c5ae2f7
ramjak <rama.jakaria@gmail.com>
2021-03-13 20:05:17 +0700

    fix: coding styles configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import BasePage, { ILink } from "./components/BasePage";
-import ROUTES, {IRoute} from './routes';
-import {IObjectMap} from "./helpers/types";
+import BasePage, { ILink } from './components/BasePage';
+import ROUTES, { IRoute } from './routes';
+import { IObjectMap } from './helpers/types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   function getLinks(o: IObjectMap<IRoute>) {
-    return Object.keys(o).map(route => ({text: route, link: o[route].path}));
+    return Object.keys(o).map((route) => ({
+      text: route,
+      link: o[route].path,
+    }));
   }
 
   // const getLinks = (routes: IRoute[]) => routes.map(route => ({ text: route. }))
   const routeLinks: ILink[] = [
-    ...(getLinks(ROUTES.free)),
-    ...(getLinks(ROUTES.nonAuthed)),
+    ...getLinks(ROUTES.free),
+    ...getLinks(ROUTES.nonAuthed),
   ];
 
-  function getRoutes(routes: IObjectMap<IRoute>) {
-    return Object.values(routes).map(route => <Route
-      key={route.path} exact={route.exact} path={route.path}
-      // @ts-ignore
-      render={(props) => <route.component {...props} />}/>);
+  function getRoutes(currRoutes: IObjectMap<IRoute>) {
+    return Object.values(currRoutes).map((route) => (
+      <Route
+        key={route.path}
+        exact={route.exact}
+        path={route.path}
+        // @ts-ignore
+        // tslint:disable-next-line jsx-no-lambda
+        render={(props) => <route.component {...props} />}
+      />
+    ));
   }
 
-  const routes = [
-    ...(getRoutes(ROUTES.nonAuthed)),
-    ...(getRoutes(ROUTES.free)),
-  ];
+  const routes = [...getRoutes(ROUTES.nonAuthed), ...getRoutes(ROUTES.free)];
   return (
     <BrowserRouter>
       <BasePage topNavLinks={routeLinks}>
