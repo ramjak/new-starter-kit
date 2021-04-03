1e3f4714da895670ea9040414a226905791ed3a0
ramjak <rama.jakaria@gmail.com>
2021-04-03 20:31:26 +0700

    feat: add MockRequestService.ts

---
+++ src/services/MockRequestService.ts
@@ -0,0 +1,102 @@
+import IRequestService, {
+  IPayload,
+  IPostRequestOptions,
+  IRequestOptions,
+} from './IRequestService';
+
+function generateRandomRealString() {
+  const randomDigits = Math.floor((Math.random() * 100) % 16) + 1;
+  return String(Math.random() * Math.pow(10, randomDigits));
+}
+
+function generateRandomIntegerString() {
+  return String(Math.floor(Math.random() * 10000000000000000));
+}
+
+function generateString(characters: string) {
+  let result = '';
+  const charactersLength = characters.length;
+  const length = Math.floor((Math.random() * 100) % 16) + 1;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function generateRandomAlphabetString() {
+  return generateString('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz');
+}
+
+function generateRandomAlphanumString() {
+  return generateString(
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  );
+}
+
+function generateRandomData() {
+  let str = '';
+  let charLeft = 2000000;
+  // let charLeft = 200;
+
+  const total = {
+    alphabets: 0,
+    reals: 0,
+    integers: 0,
+    alphanumerics: 0,
+  };
+
+  while (charLeft > 0) {
+    const randomDigit = Math.floor((Math.random() * 100) % 4);
+    let currentString = '';
+    let currentType = '';
+    switch (randomDigit) {
+      case 0:
+        currentString = generateRandomAlphabetString();
+        currentType = 'alphabets';
+        break;
+      case 1:
+        currentString = generateRandomAlphanumString();
+        currentType = 'alphanumerics';
+        break;
+      case 2:
+        currentString = generateRandomIntegerString();
+        currentType = 'integers';
+        break;
+      default:
+        currentString = generateRandomRealString();
+        currentType = 'reals';
+    }
+    if (currentString.length + 2 <= charLeft) {
+      str += currentString;
+      str += '%2C%20';
+      // @ts-ignore
+      total[currentType] += 1;
+      charLeft -= currentString.length + 2;
+    } else {
+      break;
+    }
+  }
+
+  return {
+    url: 'data:application/octet-stream;charset=utf-8,' + str,
+    summary: total,
+  };
+}
+
+export default class MockRequestService implements IRequestService {
+  public get(path: string, options?: IRequestOptions): any {
+    switch (path) {
+      case '/generate': {
+        return {
+          data: generateRandomData(),
+        };
+      }
+      default:
+        return {};
+    }
+  }
+
+  public post(path: string, payload: IPayload, options?: IPostRequestOptions): any {
+    return {};
+  }
+}
