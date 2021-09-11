6d7453b612a3223fd045b2b94b6b46fbc15fd46c
ramjak <rama.jakaria@gmail.com>
2021-09-11 13:46:01 +0700

    chore: add test for BasePage.tsx

---
+++ src/components/BasePage.test.tsx
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import React from "react";
+import { createRenderer } from "react-test-renderer/shallow";
+import BasePage from "./BasePage";
+
+it("render BasePage", () => {
+  const shallowRenderer = createRenderer();
+
+  shallowRenderer.render(
+    <BasePage
+      topNavLinks={[
+        { link: "/", text: "Root" },
+        { link: "/test", text: "Test" },
+      ]}
+    >
+      children
+    </BasePage>
+  );
+  const result = shallowRenderer.getRenderOutput();
+
+  expect(result).toMatchSnapshot();
+});
